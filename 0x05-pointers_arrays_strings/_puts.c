78aea839116fbee3ebce91422736083f777e8c9b
MUADH3137 <muadhabdishakurothowai@gmail.com>
2023-02-28 12:31:05 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/_puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ *
+ * @str: String to be printed.
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i++;
+    }
+    _putchar('\n');
+}
+
